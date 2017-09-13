c50d177cb5055568fdfc2b4a4757b99926221eec
aniket umbc <aniket.umbc@gmail.com>
2017-09-13 16:51:38 +0530

    Setup Satrt script and heroku port

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express= require('express');
 const hbs=require('hbs');
 const fs=require('fs');
+const port=process.env.PORT || 3000;
 
 var app= express ();
 
@@ -79,7 +80,7 @@ Meassage:'This is bad Data'
 });
 
 
-app.listen(3000,()=>{
+app.listen(port,()=>{
 
-    console.log("Hello console Serverup 3000");
+    console.log(`Hello console Serverup ${port}` );
 });
